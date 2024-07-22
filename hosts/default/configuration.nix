68f61a59661e55068d909c053a47954752b33993
Nikhil Suresh <nik.suresh.ns@gmail.com>
2024-07-22 18:28:44 +1000

    Clean up comments from autogenerated files

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -92,11 +92,6 @@
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
-
-  # Enable the OpenSSH daemon. services.openssh.enable = true;
-
-  # Open ports in the firewall. networking.firewall.allowedTCPPorts = [ ... ]; networking.firewall.allowedUDPPorts = [ ... ]; Or disable the firewall altogether. networking.firewall.enable = false;
-
   # It‘s perfectly fine and recommended to leave this value at the 
   # release version of the first install of this system. Before changing this value read the documentation for this option (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
