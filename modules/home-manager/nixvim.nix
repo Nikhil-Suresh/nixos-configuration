4de8eac1595c3c24a5abe8e1d8df54423f663135
Nikhil Suresh <nik.suresh.ns@gmail.com>
2024-07-22 18:23:25 +1000

    Add LSP support

--- modules/home-manager/nixvim.nix
+++ modules/home-manager/nixvim.nix
@@ -3,5 +3,26 @@
 {
   programs.nixvim = {
     enable = true;
-  };
+    defaultEditor = true;
+
+    colorschemes.tokyonight.enable = true;
+
+    plugins.airline.enable = true;
+    plugins.fugitive.enable = true;
+    plugins.harpoon.enable = true;
+
+    plugins.lsp = {
+      enable = true;
+      inlayHints = true;
+      servers = {
+        pyright.enable = true;
+	elixirls.enable = true;
+	nixd.enable = true;
+      };
+    };
+    plugins.lsp-lines.enable = true;
+    plugins.lsp-format.enable = true;
+
+    plugins.cmp.enable = true;
+    };
 }
