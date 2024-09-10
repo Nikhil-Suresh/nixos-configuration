418e3d1287d984b9ffedb6589273d05192e6b1fe
Nikhil Suresh <nikhilsuresh@hotmail.com>
2024-09-10 13:28:19 +1000

    Create configuration for laptop

---
+++ hosts/apprentice/configuration.nix
@@ -0,0 +1,12 @@
+{
+  ...
+}:
+
+{
+  # Bootloader.
+  networking.hostName = "apprentice"; # Define your hostname.
+
+  # It‘s perfectly fine and recommended to leave this value at the 
+  # release version of the first install of this system. Before changing this value read the documentation for this option (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
