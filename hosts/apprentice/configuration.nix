012fe3b88f862ca4cc51be5d2fdbfbaf14bf6835
Nikhil Suresh <nikhilsuresh@hotmail.com>
2024-09-20 13:59:36 +1000

    Fix monitor not working on laptop

--- hosts/apprentice/configuration.nix
+++ hosts/apprentice/configuration.nix
@@ -1,4 +1,5 @@
 {
+  config,
   ...
 }:
 
@@ -9,4 +10,22 @@
   # It‘s perfectly fine and recommended to leave this value at the 
   # release version of the first install of this system. Before changing this value read the documentation for this option (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+      sync.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
 }
